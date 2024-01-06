9719267527b086a3035a49b8d0ced75ecc72f2f6
sohag47 <minhazul.islamcse@gmail.com>
2024-01-06 22:00:03 +0600

    todo app db initial setup done

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Todo
+from .models import Todo, Organization, Project, Task, Group
 
+
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'status')
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'organization', 'description', 'status')
+    
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'description', 'status')
+    
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'task', 'description', 'status')
 
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'completed')
+    list_display = ('title', 'group', 'description', 'completed')
+      
 
-
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Group, GroupAdmin)
 admin.site.register(Todo, TodoAdmin)
--- backend/todo/models.py
+++ backend/todo/models.py
@@ -2,7 +2,43 @@ from django.db import models
 
 
 # Create your models here.
+class Organization(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+    
+class Project(models.Model):
+    organization = models.ForeignKey(Organization, on_delete= models.DO_NOTHING)
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete= models.DO_NOTHING)
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
+class Group(models.Model):
+    task = models.ForeignKey(Task, on_delete= models.DO_NOTHING)
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
 class Todo(models.Model):
+    group = models.ForeignKey(Group, on_delete= models.DO_NOTHING)
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
